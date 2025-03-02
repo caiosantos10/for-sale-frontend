a15fbdbed4d4f652114da9fb44d31fc5ac7a6884
Caio Santos <caiosant10@gmail.com>
2025-03-01 22:50:46 -0300

    Template do material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { SideBarComponent } from './shared/components/side-bar/side-bar.component';
-import { HeaderComponent } from './shared/components/header/header.component';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet, SideBarComponent, HeaderComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    imports: [RouterOutlet],
+    templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'for-sale-frontend';
+  title = 'MaterialPro Angular Admin Template';
 }
